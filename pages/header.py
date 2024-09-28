6e1e789036ae2b7d3ef64c9d2905380c155d0050
David P <thoughtriot211@hotmail.com>
2024-09-27 19:57:34 -0700

    updated for homework 6

---
+++ pages/header.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from time import sleep
+from pages.base_page import Page
+
+class Header(Page):
+    CART_BTN = (By.CSS_SELECTOR, "[data-test='@web/CartLink']")
+    SEARCH_FIELD = (By.ID, 'search')
+    SEARCH_BTN = (By.XPATH, "//button[@data-test='@web/Search/SearchButton']")
+
+    def search_product(self, item):
+        self.input_text(item, *self.SEARCH_FIELD)
+        self.click(*self.SEARCH_BTN)
+        sleep(6) # wait for search results page to load
+
+    def click_cart(self):
+        self.wait_to_be_clickable_click(*self.CART_BTN)
+        # self.driver.find_element(*self.CART_BTN).click()
